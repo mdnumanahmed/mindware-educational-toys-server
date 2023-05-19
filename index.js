bb937ec07b698f393486df37253476530a70a0e0
mdnumanahmed <mnumansyl@gmail.com>
2023-05-19 19:18:44 +0600

    generated jwt token and send to client

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mindware-educatinal-toy.lnffqes.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,16 +18,28 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "3d",
+      });
+      res.send({ token });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -35,12 +47,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Mind ware is running");
+});
 
-
-app.get('/', (req, res) => {
-    res.send('Mind ware is running')
-})
-
-app.listen(port, ()=>{
-    console.log(`Mindware server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Mindware server is running on port: ${port}`);
+});
